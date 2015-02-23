cd0a52dca3007062a29b773333225a587cd844ef
Dave C <dave.campofthebell@gmail.com>
2015-02-24 09:45:04 +1000

    added snotty project - dave training code

---
+++ SnottyHelloProject/src/snottyhello_pkg/SnottyProgram.java
@@ -0,0 +1,11 @@
+package snottyhello_pkg;
+
+public class SnottyProgram
+{
+
+    public static void main(String[] args)
+    {
+        SnottyHelloWorld snot = new SnottyHelloWorld(args);
+    }
+
+}
