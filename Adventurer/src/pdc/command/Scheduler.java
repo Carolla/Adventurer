876f12087b6e0b6a25d9060e88728b61edc47201
Tim Armstrong <timothyarm@gmail.com>
2015-06-28 18:16:55 -0400

    Further simplifications of Scheduler

--- Adventurer/src/pdc/command/Scheduler.java
+++ Adventurer/src/pdc/command/Scheduler.java
@@ -11,11 +11,6 @@
 
 package pdc.command;
 
-import java.util.ArrayList;
-
-import pdc.GameClock;
-
-
 /**
  * This {@code Scheduler} singleton is implemented with {@code DeltaCmdList}, a delta priority queue
  * with {@code GameClock}. The {@code Scheduler} pops a command only from the front of the queue,
@@ -31,23 +26,13 @@ import pdc.GameClock;
  *          Jul 3 2008 // Final commenting for Javadoc compliance <br>
  * 
  * @see pdc.command.DeltaCmdList
- * @see GameClock
  * @see hic.CommandParser
  * 
  */
 public class Scheduler implements Runnable
 {
-    /**
-     * Internal command token used on the DQ when one command is over and another one is needed.
-     */
-    private final String CMDEND = "intCmdEnd";
-
     /** Internal references: command events are queued here */
     private DeltaCmdList _dq = null;
-
-    /*
-     * CONSTRUCTOR(S) AND RELATED METHODS
-     */
 
     /**
      * Creates the {@code Scheduler} and its components. However, if a {@code Scheduler} does not
@@ -55,7 +40,6 @@ public class Scheduler implements Runnable
      * 
      * @param deltaCmdList bidirectional association for callbacks to get a user {@code Command} object
      *        for scheduling.
-     * @return Scheduler singleton with link to the {@code CommandParser}.
      */
     public Scheduler(DeltaCmdList deltaCmdList)
     {
@@ -63,23 +47,14 @@ public class Scheduler implements Runnable
     }
 
 
-    /*
-     * PUBLIC METHODS
-     */
-
     /**
      * This is the main loop to pop the next command and execute it, and required for the
-     * {@code Runnable} interface. The {@code Scheduler} is initialized with an {@code intEndCmd} to
-     * start off the loop. When {@code intEndCmd} is found, the {@code CommandParser} is called to
-     * get the next user command. When a user command is found, the {@code Scheduler} calls its
+     * {@code Runnable} interface. When a user command is found, the {@code Scheduler} calls its
      * {@code Command.exec()} method, which actually triggers the subcommand's {@code exec()}
      * because {@code Command} is abstract.
      */
     public void run()
     {
-        // Unless the system interrupts this thread for some reason, it continues until the Quit
-        // command
-        // If the thread is interrupted for some reason, we want to return and exit
         while (true) {
             doOneCommand();
         }
@@ -97,11 +72,7 @@ public class Scheduler implements Runnable
         _dq.insert(cmd);
     }
 
-
-    /*
-     * PRIVATE METHODS
-     */
-
+    
     /**
      * Process a single loop of Scheduler and Command parsing activity, sleeping between each cycle.
      * Generally, loop through the deltaQ, retrieving commands and calling each
@@ -110,27 +81,7 @@ public class Scheduler implements Runnable
     public void doOneCommand()
     {
         Command cmdToDo = _dq.getNextCmd();
-
-        if (cmdToDo.getName().equalsIgnoreCase(CMDEND) == true) {
-            sched(makeCmdEnd(1));
-        } else {
-            cmdToDo.exec();
-        }
-    } // end of while loop
-
-    /**
-     * Make an {@code intCmdEnd} to trigger a new user command. {@code intCmdEnd}s are used as
-     * Events to end a {@code Command}. They have a delay equal to the previous Command's duration,
-     * and no duration of their own.
-     * 
-     * @param delay this {@code intCmdEnd}'s delay
-     * @return {@code intCmdEnd} to trigger getting a new user command
-     */
-    private Command makeCmdEnd(int delay)
-    {
-        // Create endCmd to signal to get another user command
-        Command endCmd = new intCmdEnd(delay);
-        endCmd.init(new ArrayList<String>());
-        return endCmd;
+        cmdToDo.exec();
     }
+
 } // end Scheduler class
--- Adventurer/src/test/pdc/command/TestScheduler.java
+++ Adventurer/src/test/pdc/command/TestScheduler.java
@@ -1,24 +1,55 @@
+
 package test.pdc.command;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import pdc.command.Command;
 import pdc.command.DeltaCmdList;
 import pdc.command.Scheduler;
 
-public class TestScheduler {
+public class TestScheduler
+{
+    @Test(expected = NullPointerException.class)
+    public void RunningEmptyCommandThrowsException()
+    {
+        Scheduler s = new Scheduler(new DeltaCmdList());
+        s.doOneCommand();
+    }
 
-	@Test(expected=NullPointerException.class)
-	public void RunningEmptyCommandThrowsException() {
-	    Scheduler s = new Scheduler(new DeltaCmdList());
-	    s.doOneCommand();
-	}
+    @Test
+    public void RunCommandIsCommandInserted()
+    {
+        Scheduler s = new Scheduler(new DeltaCmdList());
+        CheckingCommand c = new CheckingCommand();
+        s.sched(c);
+        s.doOneCommand();
+        assertTrue(c.hasBeenRun);
+    }
 
-	public class PhonyDeltaCmdList extends DeltaCmdList {
-	    @Override
-	    public Command getNextCmd()
-	    {
-	        return new SimpleCommand();
-	    }
-	}
+    @Test
+    public void DoOneCommandOnlyDoesOneCommand()
+    {
+        Scheduler s = new Scheduler(new DeltaCmdList());
+        CheckingCommand c = new CheckingCommand();
+        CheckingCommand c2 = new CheckingCommand();
+        s.sched(c);
+        s.sched(c2);
+        s.doOneCommand();
+        assertFalse(c2.hasBeenRun);
+    }
+
+    public class CheckingCommand extends SimpleCommand
+    {
+        public boolean hasBeenRun = false;
+
+        @Override
+        public boolean exec()
+        {
+            hasBeenRun = true;
+            return true;
+        }
+
+    }
 }
