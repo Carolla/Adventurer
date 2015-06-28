bd5ff19fb374356ea6d4313f8199e7aecedf7f01
Tim Armstrong <timothyarm@gmail.com>
2015-06-28 18:08:59 -0400

    Test for running on empty command

--- Adventurer/src/test/pdc/command/TestScheduler.java
+++ Adventurer/src/test/pdc/command/TestScheduler.java
@@ -2,15 +2,23 @@ package test.pdc.command;
 
 import org.junit.Test;
 
+import pdc.command.Command;
 import pdc.command.DeltaCmdList;
 import pdc.command.Scheduler;
 
 public class TestScheduler {
 
-	@Test
-	public void test() {
+	@Test(expected=NullPointerException.class)
+	public void RunningEmptyCommandThrowsException() {
 	    Scheduler s = new Scheduler(new DeltaCmdList());
 	    s.doOneCommand();
 	}
 
+	public class PhonyDeltaCmdList extends DeltaCmdList {
+	    @Override
+	    public Command getNextCmd()
+	    {
+	        return new SimpleCommand();
+	    }
+	}
 }
