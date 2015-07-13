20537b2e7aaebccfab81972fec2d081cd206d8aa
Tim Armstrong <timothyarm@gmail.com>
2015-07-13 18:48:32 -0400

    Re-merged CmdQuit

--- Adventurer/src/pdc/command/CmdQuit.java
+++ Adventurer/src/pdc/command/CmdQuit.java
@@ -14,6 +14,8 @@ package pdc.command;
 
 import java.util.List;
 
+import civ.BuildingDisplayCiv;
+
 
 /**
  * Stops the user command dialogue and triggers the shutdown methods to end the program.
@@ -30,85 +32,98 @@ import java.util.List;
  */
 public class CmdQuit extends Command
 {
-  // THESE CONSTANTS MUST BE STATIC BECAUSE THEY ARE CALLED IN THE CONSTRUCTOR
-  /** The description of what the command does, used in the <code>help()</code> method. */
-  static final String CMD_DESCRIPTION = "End the program.";
-  /** This command starts immediately, requiring no delay. */
-  static final int DELAY = 0; // This command starts immediately on invocation
-  /** This command takes no time on the game clock. */
-  static final int DURATION = 0;
-  /** Format for usage string on input error */
-//  static final String FMT = "[takes no parameters]";
+    // THESE CONSTANTS MUST BE STATIC BECAUSE THEY ARE CALLED IN THE CONSTRUCTOR
+    /** The description of what the command does, used in the <code>help()</code> method. */
+    static final String CMD_DESCRIPTION = "End the program.";
+    /** This command starts immediately, requiring no delay. */
+    static final int DELAY = 0; // This command starts immediately on invocation
+    /** This command takes no time on the game clock. */
+    static final int DURATION = 0;
+    /** Format for usage string on input error */
+    // static final String FMT = "[takes no parameters]";
 
-  
+
     /** Error message if args are input with command */
     private final String ERRMSG_OMIT_ARGS =
             "If you want to exit the program, type \"Quit\" without any additional characters.";
 
-  // ============================================================
-  // CONSTRUCTOR(S) AND RELATED METHODS
-  // ============================================================
+    /** Error message if hero inside when command initialized */
+    private final String ERRMSG_IN_BLDG = "To quit, you must be outside.";
 
-  /** Constructor called by the CommandFactory. There is no delay nor duration. */
-  public CmdQuit()
-  {
-    super("CmdQuit", DELAY, DURATION, CMD_DESCRIPTION, null);
-  }
+    // ============================================================
+    // CONSTRUCTOR(S) AND RELATED METHODS
+    // ============================================================
 
-
-  // ============================================================
-  // PUBLIC METHODS
-  // ============================================================
-
-  /**
-   * In this case, no parms are needed.
-   * 
-   * @param args is empty for this command; implemented as required for abstract method
-   * 
-   * @return true if all worked, else returns false on input error
-   */
-  @Override
-  public boolean init(List<String> args)
-  {
-    return (args.size() == 0) ? true : false;
-  }
-
-  /**
-   * Forces the program to end.
-   * 
-   * @return false always to break out of the Scheduler loop
-   */
-  @Override
-  public boolean exec()
-  {
-    super._mfCiv.quit();
-    return true;
-  }
-  
-  /** INNER CLASS: MOCK */
-  public class MockCmdQuit
-  {
-    /** Ctor */
-    public MockCmdQuit()
-    {}
-
-    public int getDelay()
+    /** Constructor called by the CommandFactory. There is no delay nor duration. */
+    public CmdQuit()
     {
-      return CmdQuit.DELAY;
+        super("CmdQuit", DELAY, DURATION, CMD_DESCRIPTION, null);
     }
 
-    public int getDuration()
+
+    // ============================================================
+    // PUBLIC METHODS
+    // ============================================================
+
+    /**
+     * In this case, no parms are needed.
+     * 
+     * @param args is empty for this command; implemented as required for abstract method
+     * 
+     * @return true if all worked and hero is outside, else returns false on input error
+     */
+    @Override
+    public boolean init(List<String> args)
     {
-      return CmdQuit.DURATION;
-    }
-    
-    public String getDescription() {
-        return CmdQuit.CMD_DESCRIPTION;
+        BuildingDisplayCiv bdCiv = BuildingDisplayCiv.getInstance();
+        if ((args.size() == 0) && (bdCiv.isInside() == false)) {
+            return true;
+        } else if ((args.size() == 0) && (bdCiv.isInside() == true)) {
+            super._mfCiv.errorOut(ERRMSG_IN_BLDG);
+            return false;
+        } else {
+            super._mfCiv.errorOut(ERRMSG_OMIT_ARGS);
+            return false;
+        }
     }
 
-    
-  } // end MockCmdQuit class
-  
-  
+    /**
+     * Forces the program to end.
+     * 
+     * @return false always to break out of the Scheduler loop
+     */
+    @Override
+    public boolean exec()
+    {
+        super._mfCiv.quit();
+        return true;
+    }
+
+    /** INNER CLASS: MOCK */
+    public class MockCmdQuit
+    {
+        /** Ctor */
+        public MockCmdQuit()
+        {}
+
+        public int getDelay()
+        {
+            return CmdQuit.DELAY;
+        }
+
+        public int getDuration()
+        {
+            return CmdQuit.DURATION;
+        }
+
+        public String getDescription()
+        {
+            return CmdQuit.CMD_DESCRIPTION;
+        }
+
+
+    } // end MockCmdQuit class
+
+
 } // end CmdQuit class
 
