1ea445616942b4ea4487780de46128d93bd54aa5
Tim Armstrong <timothyarm@gmail.com>
2015-12-18 18:28:56 -0500

    Cleanups to remove unused things

--- Adventurer/src/test/civ/TestBuildingDisplayCiv.java
+++ Adventurer/src/test/civ/TestBuildingDisplayCiv.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.Random;
 
