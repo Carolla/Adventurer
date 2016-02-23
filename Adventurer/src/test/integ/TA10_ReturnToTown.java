5112edd055d20b7606fb1d6eb33fa70fd60d9594
Tim Armstrong <timothyarm@gmail.com>
2016-02-22 21:00:53 -0500

    Added TA10

---
+++ Adventurer/src/test/integ/TA10_ReturnToTown.java
@@ -0,0 +1,90 @@
+/**
+ * TA10_ReturnToTown.java Copyright (c) 2014, Carolla Development, Inc. All Rights Reserved
+ * 
+ * Permission to make digital or hard copies of all or parts of this work for commercial use is
+ * prohibited. To republish, to post on servers, to reuse, or to redistribute to lists, requires
+ * prior specific permission and/or a fee. Request permission to use from Carolla Development, Inc.
+ * by email: acline@carolla.com
+ */
+
+package test.integ;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import civ.BuildingDisplayCiv;
+
+/**
+ * Leave the interior of a Building.
+ * <P>
+ * Format: {@code LEAVE} moves the Bulding's exterior.
+ * 
+ * @author Alan Cline
+ * @version August 10, 2015 // original <br>
+ */
+public class TA10_ReturnToTown extends IntegrationTest
+{
+
+  private static final Random random = new Random(System.currentTimeMillis());
+  
+  @Before
+  public void setup()
+  {
+    resetBuildingState();
+  }
+
+
+  // ==========================================================
+  // Begin the tests!
+  // ==========================================================
+
+  @Test
+  public void TA10_LeaveFromInsideBuilding()
+  {
+    String building = chooseRandomBuilding();
+    _cp.receiveCommand("Enter " + building);
+
+    assertFalse(_bldgCiv.isOnTown());
+    assertTrue(_bldgCiv.isInside());
+    assertEquals(building, _bldgCiv.getCurrentBuilding());
+
+    // TEST
+    assertTrue(_cp.receiveCommand("Return"));
+
+    // Confirm Hero is no longer on town or in building
+    assertTrue(_bldgCiv.isOnTown());
+    assertFalse(_bldgCiv.isInside());
+    assertEquals("",_bldgCiv.getCurrentBuilding());
+  }
+
+  @Test
+  public void TA10_LeaveFromOutsideBuilding()
+  {
+    String building = chooseRandomBuilding();
+    _cp.receiveCommand("Approach " + building);
+
+    assertFalse(_bldgCiv.isOnTown());
+    assertFalse(_bldgCiv.isInside());
+    assertEquals(building, _bldgCiv.getCurrentBuilding());
+
+    // TEST
+    assertTrue(_cp.receiveCommand("Return"));
+
+    // Confirm Hero is no longer on town or in building
+    assertTrue(_bldgCiv.isOnTown());
+    assertFalse(_bldgCiv.isInside());
+    assertEquals("", _bldgCiv.getCurrentBuilding());
+  }
+
+
+  private String chooseRandomBuilding()
+  {
+    return BuildingDisplayCiv.DEFAULT_BUILDINGS[random.nextInt(BuildingDisplayCiv.DEFAULT_BUILDINGS.length)][0];
+  }
+}
