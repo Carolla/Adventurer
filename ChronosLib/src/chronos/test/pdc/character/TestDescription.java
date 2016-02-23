a4bb85511ce37fbca69eef470cbef7167d8e7f2f
Tim Armstrong <timothyarm@gmail.com>
2016-02-23 18:20:21 -0500

    Added some more tests for Description

--- ChronosLib/src/chronos/test/pdc/character/TestDescription.java
+++ ChronosLib/src/chronos/test/pdc/character/TestDescription.java
@@ -1,10 +1,14 @@
+
 package chronos.test.pdc.character;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import chronos.pdc.character.Description;
+import chronos.pdc.character.Gender;
 
 public class TestDescription
 {
@@ -23,5 +27,31 @@ public class TestDescription
   {
     assertEquals("A ", Description.article("Friend"));
     assertEquals("An ", Description.article("Enemy"));
+  }
+
+  @Test
+  public void baldIsBald()
+  {
+    Description desc = new Description(0, "", "bald", new Gender("Female"), 0, 0);
+    assertTrue(desc.hairDescription().contains("bald"));
+  }
+
+  @Test
+  public void hairColorIsDescribed()
+  {
+    String[] hairColors = {"red", "orange", "Blue", "Green"};
+    for (String color : hairColors) {
+      Description desc = new Description(0, "", color, new Gender("Female"), 0, 0);
+      assertTrue(desc.hairDescription().contains(color));
+    }
+  }
+  
+  @Test
+  public void twoIdenticalDescriptionsAreEqual()
+  {
+    Description desc1 = new Description(10, "red-eyed ", "red", new Gender("Female"), 50, 160);
+    Description desc2 = new Description(10, "red-eyed ", "red", new Gender("Female"), 50, 160);
+    
+    assertEquals(desc1, desc2);
   }
 }
