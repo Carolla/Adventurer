c2086a59fb403599969959f9c4b1e945dc665c01
Tim Armstrong <timothyarm@gmail.com>
2016-06-28 20:45:43 -0400

    Fixed formatting in TesTItem

--- ChronosLib/src/chronos/test/pdc/TestItem.java
+++ ChronosLib/src/chronos/test/pdc/TestItem.java
@@ -1,11 +1,9 @@
 /**
- * TestItem.java
- * Copyright (c) 2013, Carolla Development, Inc. All Rights Reserved
+ * TestItem.java Copyright (c) 2013, Carolla Development, Inc. All Rights Reserved
  *
- * Permission to make digital or hard copies of all or parts of this work for
- * commercial use is prohibited. To republish, to post on servers, to reuse,
- * or to redistribute to lists, requires prior specific permission and/or a
- * fee. Request permission to use from Carolla Development, Inc.
+ * Permission to make digital or hard copies of all or parts of this work for commercial use is
+ * prohibited. To republish, to post on servers, to reuse, or to redistribute to lists, requires
+ * prior specific permission and/or a fee. Request permission to use from Carolla Development, Inc.
  * by email: acline@carolla.com
  */
 
@@ -28,56 +26,62 @@ import chronos.pdc.Item.ItemCategory;
  *          <DD>
  *          </DL>
  */
-public class TestItem {
-	private Item item;
+public class TestItem
+{
+  private Item item;
 
-	@Before
-	public void setUp() {
-		item = new Item(ItemCategory.VALUABLES, "FakeItem", 0);
-	}
+  @Before
+  public void setUp()
+  {
+    item = new Item(ItemCategory.VALUABLES, "FakeItem", 0);
+  }
 
-	/*
-	 * ++++++++++++++++++++++++++++++++++++++++++++++++++++++ TESTS
-	 * ++++++++++++++++++++++++++++++++++++++++++++++++++++++
-	 */
+  /*
+   * ++++++++++++++++++++++++++++++++++++++++++++++++++++++ TESTS
+   * ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   */
 
-	/**
-	 * Create one of each Item and confirm
-	 * 
-	 * @Error null parms in ctor
-	 * @Error invalid parms in ctor
-	 */
-	@Test(expected = NullPointerException.class)
-	public void nullNameIsInvalid() {
-		new Item(null, null, 0);
-	}
+  /**
+   * Create one of each Item and confirm
+   * 
+   * @Error null parms in ctor
+   * @Error invalid parms in ctor
+   */
+  @Test(expected = NullPointerException.class)
+  public void nullNameIsInvalid()
+  {
+    new Item(null, null, 0);
+  }
 
-	/**
-	 * Create one of each Item and confirm
-	 * 
-	 * @Normal add and delete quantites for the Item
-	 * @Error try to drop more than the Item contains
-	 */
-	@Test
-	public void adjustQuantityWithPositiveIncreasesCount() {
-		int expQty = item.getQuantity();
+  /**
+   * Create one of each Item and confirm
+   * 
+   * @Normal add and delete quantites for the Item
+   * @Error try to drop more than the Item contains
+   */
+  @Test
+  public void adjustQuantityWithPositiveIncreasesCount()
+  {
+    int expQty = item.getQuantity();
 
-		int WINDFALL = 97;
-		assertEquals(expQty + 97, item.adjustQuantity(WINDFALL));
-	}
+    int WINDFALL = 97;
+    assertEquals(expQty + 97, item.adjustQuantity(WINDFALL));
+  }
 
-	@Test
-	public void adjustQuantityNegativeDecreaseCount() {
-		int expQty = item.adjustQuantity(120);
+  @Test
+  public void adjustQuantityNegativeDecreaseCount()
+  {
+    int expQty = item.adjustQuantity(120);
 
-		int CASHGONE = -111;
-		assertEquals(expQty + CASHGONE, item.adjustQuantity(CASHGONE));
-	}
+    int CASHGONE = -111;
+    assertEquals(expQty + CASHGONE, item.adjustQuantity(CASHGONE));
+  }
 
-	@Test
-	public void adjustQuantityWithLargeNegativeZerosCount() {
-		item.adjustQuantity(Integer.MIN_VALUE);
-		assertEquals(0, item.getQuantity());
-	}
+  @Test
+  public void adjustQuantityWithLargeNegativeZerosCount()
+  {
+    item.adjustQuantity(Integer.MIN_VALUE);
+    assertEquals(0, item.getQuantity());
+  }
 }
 // end of TestItem class
