f2eab990a5d91159718fd7663a316237a05ab61f
Al Cline <acline@carolla.com>
2016-12-23 15:50:23 -0500

    Added MyLibrary and ChronosLib file scan tests to TestSrcReader

--- QATool/QA_Tool/src/test/pdc/TestSrcReader.java
+++ QATool/QA_Tool/src/test/pdc/TestSrcReader.java
@@ -153,15 +153,17 @@ public class TestSrcReader
    @Test
    public void testScan()
    {
-      MsgCtrl.auditMsgsOn(true);
-      MsgCtrl.errorMsgsOn(true);
+      boolean state = false;
+      MsgCtrl.auditMsgsOn(state);
+      MsgCtrl.errorMsgsOn(state);
       MsgCtrl.where(this);
+      // Turn audit traisl on or off for testing
+      QAFileScan._verbose = state;
 
       // SETUP
       MockSrcReader mock = _srcRdr.new MockSrcReader();
       File srcRoot = new File(TARGET_ROOTNAME);
       // Scan a tree of files, but turn on audit messages within
-      QAFileScan._verbose = true;
 
       // RUN
       _srcRdr.scan(srcRoot);
@@ -175,6 +177,81 @@ public class TestSrcReader
                                     // SrcReader, SuiteBuilder, TargetSrcFile, TestWriter
       assertEquals(1, results[2]);  // dirs skipped: src.test
       assertEquals(3, results[3]);  // files skipped: _QAScanner, .DS_Store, ScanExclusions.txt
+
+      // TEARDOWN
+      QAFileScan._verbose = false;
+   }
+
+
+   /**
+    * @NORMAL_TEST void scan(File)
+    */
+   @Test
+   public void testScan_ChronosLib()
+   {
+      boolean state = true;
+      MsgCtrl.auditMsgsOn(state);
+      MsgCtrl.errorMsgsOn(state);
+      MsgCtrl.where(this);
+      // Turn audit traisl on or off for testing
+      QAFileScan._verbose = state;
+
+      // SETUP
+      MockSrcReader mock = _srcRdr.new MockSrcReader();
+      String ChronosLibPath = "/Projects/eChronos/ChronosLib/src/chronos";
+      File srcRoot = new File(ChronosLibPath);
+      // Scan a tree of files, but turn on audit messages within
+
+      // RUN
+      _srcRdr.scan(srcRoot);
+      _srcRdr.scanResults();
+
+      // VERIFY
+      // Results returned in this order: dirs scanned, files scanned, dir skipped, files skipped
+      int[] results = mock.getScanResults();
+      assertEquals(8, results[0]);   // dirs scanned: civ, hic, pdc, pdc.buildings, pdc.character,
+                                     // pdc.command, pdc.race, pdc.registry
+      assertEquals(60, results[1]);  // files scanned: civ(3), hic(1), pdc(9), pdc.buildings(9),
+                                     // pdc.character(10), pdc.command(8), pdc.race(10),
+                                     // pdc.registry(10)
+      assertEquals(1, results[2]);   // dirs skipped: test
+      assertEquals(0, results[3]);   // files skipped: 0
+
+      // TEARDOWN
+      QAFileScan._verbose = false;
+   }
+
+
+   /**
+    * @NORMAL_TEST void scan(File)
+    */
+   @Test
+   public void testScan_MyLibrary()
+   {
+      boolean state = false;
+      MsgCtrl.auditMsgsOn(state);
+      MsgCtrl.errorMsgsOn(state);
+      MsgCtrl.where(this);
+      // Turn audit traisl on or off for testing
+      QAFileScan._verbose = state;
+
+      // SETUP
+      MockSrcReader mock = _srcRdr.new MockSrcReader();
+      String MyLibraryPath = "/Projects/eChronos/MyLibrary/src/mylib";
+      File srcRoot = new File(MyLibraryPath);
+      // Scan a tree of files, but turn on audit messages within
+
+      // RUN
+      _srcRdr.scan(srcRoot);
+      _srcRdr.scanResults();
+
+      // VERIFY
+      // Results returned in this order: dirs scanned, files scanned, dir skipped, files skipped
+      int[] results = mock.getScanResults();
+      assertEquals(4, results[0]);  // dirs scanned: dmc, hic, libResources, pdc
+      assertEquals(9, results[1]);  // files scanned: mylib(3), dmc(2), hic(1), pdc(3)
+      assertEquals(1, results[2]);  // dirs skipped: mylib.test
+      assertEquals(5, results[3]);  // files skipped: libResources(5)
 
       // TEARDOWN
       QAFileScan._verbose = false;
