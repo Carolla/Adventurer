fc75d78fd858cf590e16264bd92b99c415215557
Al Cline <acline@carolla.com>
2018-04-08 16:08:09 -0400

    One more file

---
+++ Adventurer/src/civ/IMainActionPanel.java
@@ -0,0 +1,21 @@
+/**
+ * IMainActionPanel.java Copyright (c) 2018, Alan Cline. All Rights Reserved
+ * 
+ * Permission to make digital or hard copies of all or parts of this work for commercial use is
+ * prohibited. To republish, to post on servers, to reuse, or to redistribute to lists,
+ * requires prior specific permission and/or a fee. Request permission to use by email:
+ * acline@wowway.com.
+ */
+
+package civ;
+
+/**
+ * Used as socket for production and testing code to allow civ classes to support both.
+ * 
+ * @author Al Cline
+ * @version Feb 21, 2015 // original <br>
+ */
+public interface IMainActionPanel
+{
+  
+} // end of IMainActionPanel
