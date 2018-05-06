f9256874577f75a6241d1a67ed6c83e8855a3756
Al Cline <acline@carolla.com>
2018-05-06 19:25:15 -0400

    Added MersenneTwister to replace Java randomizer from standard library

--- MyLibrary/src/mylib/pdc/MetaDie.java
+++ MyLibrary/src/mylib/pdc/MetaDie.java
@@ -5,6 +5,7 @@
 
 package mylib.pdc;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,35 +15,36 @@ import mylib.ApplicationException;
 
 
 /**
- * A custom random generator for rolling dice for standard d20 gaming system rules, including
+ * Simulates rolling dice in various formats for standard d20 gaming system rules, e.g.
  * Chronos.
  * <p>
  * {@code MetaDie} has three core methods:
  * <ul>
- * <li>{@code getRandom(int minRange, int maxRange)}, which selects a non-zero positive number from
- * a <i> linear </i> random population within the range, defined by the given limits (inclusive).
- * Use this method only when positive (non-zero) numbers are desired; e.g. rolling dice "2d6". Do
- * not use it for negative offsets, such as "2d6-10". See the method details for other limitations.
+ * <li>{@code getRandom(int minRange, int maxRange)}, which selects a non-zero positive number
+ * from a <i> linear </i> random population within the range, defined by the given limits
+ * (inclusive). Use this method only when positive (non-zero) numbers are desired; e.g. rolling
+ * dice "2d6". Do not use it for negative offsets, such as "2d6-10". See the method details for
+ * other limitations.
  * <P>
  * </li>
- * <li>{@code getRandomGaussian(double maxRange)}, which selects a number from a Gaussian
- * <i> normal </i> random population from 0 to maxRange, inclusive. Use this method whenever
- * non-positive values are possible, or 0 falls within the desired output range. This is the method
- * to use for example, when needing an interval of [0, 20] lb above an average when calculating
- * weight.
+ * <li>{@code getRandomGaussian(double maxRange)}, which selects a number from a Gaussian <i>
+ * normal </i> random population from 0 to maxRange, inclusive. Use this method whenever
+ * non-positive values are possible, or 0 falls within the desired output range. This is the
+ * method to use for example, when needing an interval of [0, 20] pounds above an average when
+ * calculating weight.
  * <P>
  * </li>
- * <li>{@code roll(int nbrDice, int nbrSides)}, which returns the sum of random numbers by adding
- * the given dice together, as in "2d6". A call to this method adds the rolled value from multiple
- * {@code nbrDice} linear distributions to produce a Gaussian distribution. This method should
- * produce the same results as {@code getRandomGaussian(double)} but tests have not been conducted
- * yet to confirm that.
+ * <li>{@code roll(int nbrDice, int nbrSides)}, which returns the sum of random numbers by
+ * adding the given dice together, as in "2d6". A call to this method adds the rolled value
+ * from multiple {@code nbrDice} linear distributions to produce a Gaussian distribution. This
+ * method is best used when generating positive Gaussian distributions, such as rolling a
+ * Character's prime traits.
  * <P>
  * </li>
  * </ul>
  * <p>
- * {@code MetaDie} can be created either in debug mode (repeatable numeric sequences) or with the                        
- * standard randomizer (non-repeatable). <br>
+ * {@code MetaDie} can be created either in debug mode (repeatable numeric sequences) or with
+ * the standard randomizer (non-repeatable). <br>
  * 
  *
  * @author Alan Cline
@@ -53,16 +55,21 @@ import mylib.ApplicationException;
  *          Aug 15, 2017 // repaired roll(int, int) for 0-based numbers <br>
  *          Aug 18, 2017 // repaired getRandom(int, int) for 1-based numbers <br>
  *          Mar 17, 2018 // getting isOdd() working with new Eclipse and JUnit 5 <br>
- * 
- * @see java.util.Random
+ *          Apr 16, 2018 // replaced Java Random class with Apache MersenneTwister class <br>
  */
 public class MetaDie
 {
-  /** Standard random generator class that powers the <code>MetaDie</code> class */
+  /** Specialized Apache random generator class that powers the <code>MetaDie</code> class */
   private MersenneTwister _generator;
 
-  /** 68% of the population is centered around the mean, within 1 standard deviation */
+  /** 34% of the population centered one side of the mean, within 1 sigma. */
   public static final double SIGMA = 0.3413; // positive side of the mean
+  /** 68% of the population is centered around the mean, within 1 standard deviation */
+  public static final double ONE_SIGMA = 0.6827;
+  /** 95% of the population (additional 27.18%) is within the second standard deviation */
+  public static final double TWO_SIGMA = 0.9545;
+  /** Smallest population of numbers on which to properly apply Guassian statistics */
+  private static int VALID_STAT_SIZE = 25;
 
   /** Default trait range for a Hero, before gender or racial adjustments */
   private final int MIN_TRAIT = 8;
@@ -84,15 +91,14 @@ public class MetaDie
    */
   public MetaDie()
   {
-    // _generator = new Random(System.currentTimeMillis());
     _generator = new MersenneTwister();
   }
 
 
   /**
-   * Creates a Random class using an input value as a seed. This object pre-defines a sequence of
-   * random numbers, but the same seed generates the same sequence of numbers, so this version is
-   * useful for debugging.
+   * Creates a Random class using an input value as a seed. This object pre-defines a sequence
+   * of random numbers, but the same seed generates the same sequence of numbers, so this
+   * version is useful for debugging.
    *
    * @param seed a number that determines the number sequence generated.
    */
@@ -107,16 +113,13 @@ public class MetaDie
   // ============================================================================================
 
   /**
-   * Returns a single random number that falls within the linear range [minRange, maxRange]. Ranges
-   * represent positive linear distribution: maxRange must be greater than the minRange, and both
-   * must be non-negative.
-   * <P>
-   * WARNING: Average values for a set of high minRanges vary from the expected average; that is,
-   * getRandom(1, 100) may produce a difference from the true average by almost 2%
+   * Returns a single random number that falls within the linear range [minRange, maxRange].
+   * Ranges represent positive linear distribution: maxRange must be greater than the minRange,
+   * and both must be non-negative.
    *
    * @param minRange the smallest positive number requested greater than 0
-   * @param maxRange the largest positive number requested (exclusive)
-   * @return a random number within the range (always greater than 0)
+   * @param maxRange the largest positive number requested
+   * @return a random number within the range inclusive (always greater than 0)
    * @throws IllegalArgumentException if the input parms are negative or out of range
    */
   public int getRandom(int minRange, int maxRange) throws IllegalArgumentException
@@ -127,27 +130,113 @@ public class MetaDie
     if (minRange >= maxRange) {
       throw new IllegalArgumentException("Max range must be larger than min range");
     }
-    // Convert from 0-based to 1-based distribution; bound is non-inclusive for the random value
+    // Convert from 0-based to 1-based distribution
     int bound = maxRange - minRange + 1;
     return _generator.nextInt(bound) + minRange;
-    // nextInt seems to have problems with large numbers, e.g., 100. Try this Math method instead
-    // return (int) Math.round(Math.random() * maxRange);
   }
 
-  
-  /** Generate a random boolean as true or false.
+
+//  /***
+//   * Theoretically, an infinite number of values in the input {@code population} should contain
+//   * 68% of its values in the range within one standard deviation (sigma) about the mean. It
+//   * should contains 95% of its values in the range of two standard deviations about the mean,
+//   * which means 27% each on either side and outside the first-sigma range. The remaining
+//   * values (2.5%) are split evenly on either side of the second-sigma range.
+//   * <P>
+//   * For example, for a large set of numbers in the range of [8, 18], the population has a mean
+//   * = 13.0 and sigma = 1.67. Sixty-five percent should fall within [11.3, 14.7]. Another 27%
+//   * should fall within [9.7, 11.3) and 27% within (14.7, 16.3]. The other 5% of the values
+//   * should fall between [8, 9.7) and (16.3, 18].
+//   * <P>
+//   * Practically, this would be difficult to measure or verify for randomly generated numbers,
+//   * especially for small populations. {@code isGaussian()} will verify that the majority of
+//   * values occur within the first-sigma range, and the second most populous values both
+//   * second-sigma ranges. The remaining values from those are split evenly on either side of
+//   * the second-sigma range, should contain the least populous number of values. If the curve
+//   * is not strictly Gaussian, it at least will be triangular, which is good enough for our
+//   * purposes, i.e., game character generation.
+//   * <P>
+//   * <i> Implementation</i>: The mean is found from (max + min)/2.0; one sigma is defined as
+//   * (max-min)/6.0. The population is distributed within three buckets. Bucket 1 contains all
+//   * data within the first-sigma range; bucket 2 contains all value within the second-sigma
+//   * range but outside the first-sigma range, and bucket 3 contains the remaining data. The
+//   * actual percentages may not always occur as they should theoretically, but at least the
+//   * buckets should contain the most in bucket 1, the least in bucket 3, and a medium number in
+//   * bucket 2.
+//   * <P>
+//   * LIMITATION: The population must have at least 25 values in it, else returns false. <br>
+//   * LIMITATION: The population must have at least 300 values before a normallity test can be
+//   * accurate.
+//   *
+//   * @param population of numbers to process
+//   * @param min smallest value in the population to help define the range, average, and sigma
+//   * @param max largest value in the population to help define the range, average, and sigma
+//   * @return true if more number are within 1 sigma, second most numbers within 2 sigma (first
+//   *         sigma numers removed) and the least in bucket 3.
+//   */
+//  public boolean isGaussian(int[] population, int min, int max)
+//  {
+//    // Guard against too-small populations
+//    if (population.length < VALID_STAT_SIZE) {
+//      return false;
+//    }
+//    ArrayList<Double> bucket1 = new ArrayList<Double>();
+//    ArrayList<Double> bucket2 = new ArrayList<Double>();
+//    ArrayList<Double> bucket3 = new ArrayList<Double>();
+//
+//    // Define the bucket boundaries
+//    double sigma = (max - min) / 6.0;
+//    double avg = (max + min) / 2.0;
+//    double minRange1 = avg - sigma;
+//    double maxRange1 = avg + sigma;
+//    double minRange2 = minRange1 - sigma;
+//    double maxRange2 = maxRange1 + sigma;
+//    // Populate the buckets; note that the order of the comparison is important
+//    for (int k = 0; k < population.length; k++) {
+//      double target = population[k];
+//      // Place values close to the mean in bucket 1
+//      if ((target >= minRange1) && (target <= maxRange1)) {
+//        bucket1.add(target);
+//        // System.out.println("\t target = " + target + " => [11.3, 14.7]");
+//      } else {
+//        // Place values in the second-sigma split ranges into bucket 2
+//        boolean upperRange = ((target > maxRange1) && (target <= maxRange2));
+//        boolean lowerRange = ((target >= minRange2) && (target < minRange1));
+//        if (upperRange || lowerRange) {
+//          bucket2.add(target);
+//          // System.out.println("\t target = " + target + " => [9.7, 11.3) || (14.7, 16.3]");
+//        } else {
+//          bucket3.add(target);
+//          // System.out.println("\t target = " + target + " => [8, 9.7) || 16.3, 18]");
+//        }
+//      }
+//    }
+//    // Verify that the proper percentages are in the buckets
+//    int firstSigma = bucket1.size();
+//    int secondSigma = bucket2.size();
+//    int thirdSigma = bucket3.size();
+//    System.out.print("\tbucket 1 = " + firstSigma);
+//    System.out.print("\tbucket 2 = " + secondSigma);
+//    System.out.print("\tbucket 3 = " + thirdSigma);
+//    return ((firstSigma > secondSigma) && (secondSigma > thirdSigma));
+//  }
+
+
+  /**
+   * Generate a random boolean as true or false.
+   * 
    * @return random true or false value
    */
   public boolean isOdd()
   {
-    return _generator.nextBoolean(); 
+    return _generator.nextBoolean();
   }
 
-  
+
   /**
-   * Returns the sum of rolling the given number of dice, which is similar to selecting a number
-   * from a normal (Gaussian) population that has the given dice characteristics. If nbrDice = 1,
-   * then this method returns a (linear) number from 1 to nbrSides.
+   * Returns the sum of rolling the given number of dice, which is similar to selecting a
+   * number from a normal (Gaussian) population that has the given dice characteristics. If
+   * nbrDice = 1, then this method returns a (linear) number from 1 to nbrSides.
    * 
    * @param nbrDice number of rolls to sum (minimum of 1)
    * @param nbrSides max range of each roll (number of sides of die; minimum of 2)
@@ -173,10 +262,10 @@ public class MetaDie
 
 
   /**
-   * Pull a random number from a Gaussian population to add or subtract from the given average. The
-   * range is calculated from the low and average parms. this method must transform from a Gaussian
-   * average of 0 and a Gaussian range slightly over 3 to the average and range signified by the
-   * input parms.
+   * Pull a random number from a Gaussian population to add or subtract from the given average.
+   * The range is calculated from the low and average parms. this method must transform from a
+   * Gaussian average of 0 and a Gaussian range slightly over 3 to the average and range
+   * signified by the input parms.
    * 
    * @param low the low end of the range
    * @param average the middle of the range
@@ -208,12 +297,12 @@ public class MetaDie
 
 
   /**
-   * Convert a dice notation string to two numbers, then roll the dice as indicated by the string.
-   * This method should only be called if the lowest possible value is positive. In cases where a
-   * non-positive result may occur (e.g., 2d4-2), an exception is thrown directing the caller to
-   * call {@code getRandomGaussian()}. <br>
-   * Notation format: {[n][n] 'd' n[n][n] ['+' | '-'] n]}, that is, 1-100 required letter 'd', and
-   * 1-100, e.g. 2d10, 1d8+1, or 2d4-1. Optional +n to provide minimums. <br>
+   * Convert a dice notation string to two numbers, then roll the dice as indicated by the
+   * string. This method should only be called if the lowest possible value is positive. In
+   * cases where a non-positive result may occur (e.g., 2d4-2), an exception is thrown
+   * directing the caller to call {@code getRandomGaussian()}. <br>
+   * Notation format: {[n][n] 'd' n[n][n] ['+' | '-'] n]}, that is, 1-100 required letter 'd',
+   * and 1-100, e.g. 2d10, 1d8+1, or 2d4-1. Optional +n to provide minimums. <br>
    * 
    * @param notation format as explained above
    * @return the sum of the rolled dice; else 1 if a zero occurred
@@ -243,13 +332,15 @@ public class MetaDie
 
 
   /**
-   * Returns a single trait using the 4d6-1 algorithm: Four d6 dice are rolled, and the lowest die
-   * roll is removed from the list, giving an average of 12.5 for a Person's trait. Imposes the
-   * constraint that all traits must be greater than a certain minimum value MIN_TRAIT.
+   * Returns a single trait using the "4d6-1" algorithm: Four d6 dice are rolled, and the
+   * lowest die roll is removed from the list, giving an average of 11.5 for a Person's trait.
+   * Imposes the constraint that all traits must be within a specified range inclusive.
    * 
-   * @return value from 8 (constrained) to 18 (three 6's)
+   * @param minTrait the lower boundary of the character trait allowed
+   * @param maxTrait the upper boundary of the character trait allowed
+   * @return 4d6 - lowest roll within given range [minTrait, maxTrait]
    */
-  public int rollCharTrait()
+  public int rollCharTrait(int minTrait, int maxTrait)
   {
     // Collect the rolls
     int[] trait = new int[NBR_ROLLS];
@@ -271,16 +362,15 @@ public class MetaDie
     sum -= min;
 
     // If char trait is out of bounds, reroll trait
-    if ((sum < MIN_TRAIT) || (sum > MAX_TRAIT)) {
-      sum = rollCharTrait();
+    if ((sum < minTrait) || (sum > maxTrait)) {
+      sum = rollCharTrait(minTrait, maxTrait);
     }
     return sum;
   }
 
 
   /**
-   * Returns a linear random number in terms of percent. {@code getRandom()} seems to trend badly
-   * for large numbers, so this percent is generated by a d10 * 10 + d10
+   * Returns a linear random number in terms of percent, generated by {@code d10 * 10 + d10}.
    * 
    * @return value from 1 to 100
    */
@@ -292,13 +382,12 @@ public class MetaDie
     int tens = 10 * (getRandom(1, 10) - 1);
     int result = tens + digits + 1;
     return result;
-    // Force a non-zero value to be returned
-    //return (result == 0) ? 1 : result;
   }
 
 
   /**
-   * Rolls six random numbers for Hero traits, each constrained to fall within 8 and 18, inclusive
+   * Rolls six random numbers for Hero traits, each constrained to fall within 8 and 18,
+   * inclusive
    * 
    * @return the six traits for the Hero
    */
@@ -306,7 +395,7 @@ public class MetaDie
   {
     int traits[] = new int[NBR_TRAITS];
     for (int i = 0; i < traits.length; i++) {
-      traits[i] = rollCharTrait();
+      traits[i] = rollCharTrait(MIN_TRAIT, MAX_TRAIT);
     }
     return traits;
   }
